d5f5570eaec6df35d39d7a4459f2b607af36126f
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-04-06 17:30:39 +0300

    added projects

--- build/bundle.js
+++
@@ -1,7 +0,0 @@
-var hello = 2;
-var hello = 3;
-var hello = 2;
-var hello = 3;
-
-var hello = 2;
-var hello = 3;
--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,17 @@ var gulp        = require('gulp'),
     gutil       = require('gulp-util')
     git         = require('gulp-git'),
     sass        = require('gulp-sass'),
+    clean       = require('gulp-clean'),
+    rename      = require('gulp-rename'),
     eslint      = require('gulp-eslint'),
     concat      = require('gulp-concat'),
+    replace     = require('gulp-replace'),
+    markdown    = require('gulp-markdown'),
     cachebust   = require('gulp-cache-bust'),
-    shortid     = require('shortid');
+    markdownpdf = require('gulp-markdown-pdf'),
+    shortid     = require('shortid'),
+    marked      = require('marked'),
+    fs          = require('fs');
 
 gulp.task('lint-js', function () {
     return gulp.src(['src/*.js'])
@@ -32,16 +39,29 @@ gulp.task('build-js', function() {
 });
 
 gulp.task('build-html', function() {
+    var projectsmd = marked( 
+        fs.readFileSync('data/projects.md').toString('utf8') 
+    );
+
     gulp.src('src/html/*.html')
         .pipe(cachebust({
             type: 'timestamp'
         }))
+        .pipe(replace(/\#projects\.md/mig, projectsmd))
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('build', ['build-css', 'lint-js', 'build-js', 'build-html'], function() {});
+gulp.task('build-cv-pdf', function() {
+    return gulp.src('data/resume.md')
+        .pipe(markdownpdf())
+        .pipe(rename('Vladislav_Gritsenko.pdf'))
+        .pipe(gulp.dest('data'));
+});
 
-gulp.task('minify', function() {});
+gulp.task('build', ['build-css', 'lint-js', 'build-js', 'build-html']);
+
+// gulp.task('minify', function() {});
+// gulp.task('build-markdown', function() {});
 
 gulp.task('watch', ['build'], function() {
     gulp.watch('src/scripts/*.js',   ['lint-js', 'build-js']);
@@ -49,11 +69,11 @@ gulp.task('watch', ['build'], function() {
     gulp.watch('src/html/*.html', ['build-html']);
 });
 
-gulp.task('publish', ['build', 'minify'], function() {
-    gulp.src('./*')
-        .pipe(git.add())
-        .pipe(git.commit('pub.: ' + shortid()))
-        .pipe(git.push('origin', 'master'), function (err) {
-            if (err) throw err;
-        });
-});
+// gulp.task('publish', ['build'], function() {
+//     gulp.src('./*')
+//         .pipe(git.add())
+//         .pipe(git.commit('pub.: ' + shortid()))
+//         .pipe(git.push('origin', 'master'), function (err) {
+//             if (err) throw err;
+//         });
+// });
